9dd68a83fc75a7a99088d5c02c55e396131df601
araguaci <araguaci@gmail.com>
2025-08-18 15:10:44 -0300

    Geoengenharia: Projetos Recentes, Riscos e Implicações Globais

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,40 +1,20 @@
 const swconf = {
-  
-    cacheName: 'chirpy-1755538104',resources: [
-      '/assets/css/jekyll-theme-chirpy.css',
-      '/',
-      
-        '/5w2h/',
-      
-        '/geoengenharia/',
-      
-        '/timeline/',
-      
-        '/pinect/',
-      
-        '/categories/',
-      
-        '/rss/',
-      
-        '/tags/',
-      
-        '/about/',
-      
-
-      
-      
-        '/assets/js/dist/app.min.js',
-        '/assets/js/dist/categories.min.js',
-        '/assets/js/dist/commons.min.js',
-        '/assets/js/dist/home.min.js',
-        '/assets/js/dist/misc.min.js',
-        '/assets/js/dist/page.min.js',
-        '/assets/js/dist/post.min.js',
-        '/assets/js/dist/pvreport.min.js'
-    ],denyPaths: [
-      
-    ],
-    purge: false
-  
+  cacheName: 'chirpy-1717010746',
+  resources: [
+    '/assets/css/jekyll-theme-chirpy.css',
+    '/',
+    '/categories/',
+    '/tags/',
+    '/timeline/',
+    '/about/',
+    '/assets/js/dist/app.min.js',
+    '/assets/js/dist/categories.min.js',
+    '/assets/js/dist/commons.min.js',
+    '/assets/js/dist/home.min.js',
+    '/assets/js/dist/misc.min.js',
+    '/assets/js/dist/page.min.js',
+    '/assets/js/dist/post.min.js'
+  ],
+  denyPaths: [],
+  purge: false
 };
-
